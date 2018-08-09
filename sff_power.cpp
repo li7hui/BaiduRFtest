e1840922f0153e52e8d36282506f504894199364
HuiLi <lihui@indiana.edu>
2018-08-09 06:31:44 +0000

    six commit

--- sff_power.cpp
+++ sff_power.cpp
@@ -15,11 +15,11 @@
 using namespace std; 
 #include<gtest/gtest.h>
 
-TEST(LED,location1)
+TEST(SFF,power1)
 {
 	char cbuf[128];
 	char rbuf[128];
-	sprintf(cbuf,"cat /sys/switch/led/location");
+	sprintf(cbuf,"cat /sys/switch/sff/power");
 	FILE *stream = popen(cbuf,"r");
 	fread(rbuf,sizeof(rbuf),sizeof(char),stream);
 	printf("%s",rbuf);	
